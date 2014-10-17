4b12d027a0d1822f66e727643fe1c58dc9d4d15c
Kyle Harrison <khwebmail@gmail.com>
2014-10-17 19:26:33 +0100

    Initial Changes for Nameless Rom

--- nameless_u8833.mk
+++ nameless_u8833.mk
@@ -14,22 +14,28 @@
 # limitations under the License.
 #
 
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/slim/config/gsm.mk)
-
-# Inherit some common CM stuff
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
-
-# Inherit device configuration
-$(call inherit-product, device/huawei/u8833/full_u8833.mk)
-
 # Correct boot animation size for the screen
 TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
 
-# Device name
-PRODUCT_NAME := slim_u8833
-PRODUCT_DEVICE := u8833
+# APNs
+$(call inherit-product, vendor/nameless/config/apns.mk)
 
-PRODUCT_PACKAGES += \
-    Torch
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/nameless/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/huawei/u8833/full_u8833.mk)
+
+# Release name and versioning
+PRODUCT_RELEASE_NAME := Huawei Y300
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := u8833
+PRODUCT_NAME := nameless_u8833
+PRODUCT_BRAND := Huawei
+PRODUCT_MODEL := Huawei Y300
+PRODUCT_MANUFACTURER := Huawei
